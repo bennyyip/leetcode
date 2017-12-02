8c1ef8d66d79367290600a7e81dfc60d9dc81c86
BennyYip <yebenmy@protonmail.com>
2017-12-02 10:54:09 +0800

    3/635

---
+++ longest-substring-without-repeating-characters.cc
@@ -0,0 +1,13 @@
+class Solution {
+public:
+  int lengthOfLongestSubstring(string s) {
+    int r = 0, m = -1;
+    vector<int> c(127, -1);
+    for (int i = 0; i < s.size(); ++i) {
+      m = max(m, c[s[i]]);
+      r = max(r, i - m);
+      c[s[i]] = i;
+    }
+    return r;
+  }
+};

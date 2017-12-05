4614e7009ee4f36bf3f184fc2fc64754e5cae9a2
BennyYip <yebenmy@protonmail.com>
2017-12-05 22:11:13 +0800

    5/635

---
+++ longest-palindromic-substring.cc
@@ -0,0 +1,29 @@
+// TODO: Manacher's Algorithm, O(n) algorithm to find longest palindrome
+
+class Solution {
+public:
+  /* O(n^2) */
+  string longestPalindrome(string s) {
+    int l = 0, r = 0;
+    for (size_t i = 0; i < s.size(); i++) {
+      int len1 = expandFromCenter(s, i, i);
+      int len2 = expandFromCenter(s, i, i + 1);
+      int len = max(len1, len2);
+      /* abba  i = 1, len = 4 */
+      /* ababa i = 2, len = 5 */
+      if (len > r - l + 1) {
+        l = i - (len - 1) / 2;
+        r = i + len / 2;
+      }
+    }
+    return s.substr(l, r - l + 1);
+  }
+
+  int expandFromCenter(const string& s, int l, int r) {
+    while (l >= 0 && r < s.size() && s[l] == s[r]) {
+      l--;
+      r++;
+    }
+    return r - l - 1;
+  }
+};

09b8e12c7637c6a56dbae9c0d2097d2eb2da6577
BennyYip <yebenmy@protonmail.com>
2017-12-06 23:57:23 +0800

    6/635

---
+++ zigzag-conversion.cc
@@ -0,0 +1,16 @@
+class Solution {
+public:
+  string convert(string s, int numRows) {
+    if (numRows <= 1)
+      return s;
+    string r;
+    for (size_t i = 0; i < numRows; i++) {
+      for (size_t j = i; j < s.size(); j += 2 * numRows - 2) {
+        r += s[j];
+        if (i > 0 && i < numRows - 1 && j + 2 * (numRows - i - 1) < s.size())
+          r += s[j + 2 * (numRows - i - 1)];
+      }
+    }
+    return r;
+  }
+};
